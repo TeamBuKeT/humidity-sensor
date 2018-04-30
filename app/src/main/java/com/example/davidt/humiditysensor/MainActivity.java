24efeae941c335071eedfd331ba1e9f7348de210
diez69 <davidtran69@hotmail.fr>
2018-04-30 17:05:56 +0200

    [FEAT] MainActivity

--- app/src/main/java/com/example/davidt/humiditysensor/MainActivity.java
+++ app/src/main/java/com/example/davidt/humiditysensor/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.davidt.humiditysensor;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.BtnWater);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onClickWater();
+            }
+        });
+    }
+
+    private void onClickWater() {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+        builder.setTitle("Arrosage")
+                .setMessage("Etes-vous sur de vouloir arroser la plante ?")
+                .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //TODO : Envoyer l'ordre d'arroser à Arduino
+                        Toast.makeText(getBaseContext(), "Arrosage en cours !",
+                                Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
