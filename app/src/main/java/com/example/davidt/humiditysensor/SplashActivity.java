e76d4d31e75592e65cfe4571ae43c49560d3eed9
diez69 <davidtran69@hotmail.fr>
2018-05-03 15:37:04 +0200

    [FEAT] Firebase + Check Wifi

--- app/src/main/java/com/example/davidt/humiditysensor/SplashActivity.java
+++ app/src/main/java/com/example/davidt/humiditysensor/SplashActivity.java
@@ -1,24 +1,29 @@
 package com.example.davidt.humiditysensor;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class SplashActivity extends Activity {
+
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
         Window window = getWindow();
         window.setFormat(PixelFormat.RGBA_8888);
     }
     /** Called when the activity is first created. */
-    Thread splashTread;
+    Thread splashTread, finishAppThread;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,11 +49,10 @@ public class SplashActivity extends Activity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 4500) {
+                    while (waited < 4000) {
                         sleep(100);
                         waited += 100;
                     }
-
                     Intent intent = new Intent(SplashActivity.this,
                             MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
@@ -62,8 +66,37 @@ public class SplashActivity extends Activity {
 
             }
         };
-        splashTread.start();
 
+        finishAppThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 4000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mWifi.isConnected()) {
+            splashTread.start();
+        }
+        else {
+            finishAppThread.start();
+            Toast.makeText(getBaseContext(), "Aucune connection Wifi ! Veuillez vous connecter à un réseau Wifi puis relancer l'application.",
+                    Toast.LENGTH_LONG).show();
+
+        }
     }
-
 }
